e8b6105121f7b51753ef3ab161abb9ba8d5bee5e
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2023-05-13 13:52:51 -0600

    mas datasets

--- src/dataset/get_features.py
+++ src/dataset/get_features.py
@@ -4,7 +4,7 @@ import numpy as np
 import requests
 
 # Replace here the TOKEN from api
-TOKEN = 'BQAWecaSq0plGUufi4FK5bKqXvM_EzsO3UfU0ByLX-CqL3MMwJqTnYMtSZxFX2VmUv5lbo91JBbJGvT2eLvrhI0-xfmMV3ur-iX1To_PjsKHZj9GziBs'
+TOKEN = ''
 headers = {'Authorization': 'Bearer  ' + TOKEN}
 
 # Make a request for features of track
@@ -21,15 +21,26 @@ def get_features_dataset(path_data):
     columns = df.columns
     df = df[df['num_tracks'] >= 200]
     d = df.to_numpy()
+    t = d.shape[0]
     for i in range(d.shape[0]):
         playlist = d[i]
         for j in range(len(playlist[7])):
             track = playlist[7][j]
             features = get_features_track(track['track_uri'])
             d[i][7][j]['features'] = features
+            print (f'dataset {path_data} - {i}|{j}  /{t}')
     new_df = pd.DataFrame(d, columns=columns)
     new_path = path_data.replace('.json', '-features.json')
     new_df.to_json(new_path, orient='split')
 
-# Get the features from  mpd.slice.0-999.json file and save it as mpd.slice.0-999-features.json
-get_features_dataset('mpd.slice.0-999.json')
+# Get the features from datasets list and save it as mpd.slice.0-999-features.json
+# 34 1000-1999
+# 29 2000-2999
+# 37 3000-3999
+# 32 4000-4999
+# 38 5000-5999
+get_features_dataset('mpd.slice.1000-1999.json')
+get_features_dataset('mpd.slice.2000-2999.json')
+get_features_dataset('mpd.slice.3000-3999.json')
+get_features_dataset('mpd.slice.4000-4999.json')
+get_features_dataset('mpd.slice.5000-5999.json')
